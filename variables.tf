2c3dc42f01b0c0de5b63593081a226f90d96b30d
easec <mats.johannesson@easec.se>
2021-12-10 16:00:39 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  description = "The project to use for unique resource naming"
+  default     = "easec"
+  type        = string
+}
+
+variable "ssh_keypair" {
+  description = "SSH keypair to use for EC2 instance"
+  default     = null
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "eu-west-1"
+  type        = string
+}
