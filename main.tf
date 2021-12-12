2947954c284d8e099cb1fca5d61b90ebabf70640
easec <mats.johannesson@easec.se>
2021-12-12 16:06:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ module "networking" {
   source    = "./modules/networking"
   project   = var.project
 } 
+
+  module "database" {
+  source    = "./modules/database"
+  project = var.project
+
+  vpc = module.networking.vpc
+  sg  = module.networking.sg
+}
