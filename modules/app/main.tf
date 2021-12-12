148fd0c3c864c294b28af5cc4dd72eac3eb9c86f
easec <mats.johannesson@easec.se>
2021-12-12 17:14:42 +0100

    Create main.tf

---
+++ modules/app/main.tf
@@ -0,0 +1,24 @@
+data "cloudinit_config" "config" {
+  gzip = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/app_config.yaml", var.db_config)
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [
+      "hvm" ]
+  }
+  owners = [
+    "099720109477" ]
+}
