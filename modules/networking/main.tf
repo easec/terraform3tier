0c8127c7e35bd55e3befebd23d70def4016a75d9
easec <mats.johannesson@easec.se>
2021-12-12 11:28:19 +0100

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -15,3 +15,80 @@ module "vpc" {
   enable_nat_gateway               = false
   single_nat_gateway               = false
 }
+
+module "web_alb_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+
+  name        = "web-alb-sg"
+  description = "Security group for web tier app load balancer in VPC"
+  vpc_id      = module.vpc.vpc_id
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
+module "web_server_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+
+  name        = "web-server-sg"
+  description = "Security group for web servers in VPC"
+  vpc_id      = module.vpc.vpc_id
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.web_alb_sg.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+}
+
+module "app_alb_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+
+  name        = "app-alb-sg"
+  description = "Security group for app tier alb in VPC"
+  vpc_id      = module.vpc.vpc_id
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.web_server_sg.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+}
+
+module "app_server_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+
+  name        = "app-server-sg"
+  description = "Security group for app servers in VPC"
+  vpc_id      = module.vpc.vpc_id
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.app_alb_sg.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+}
+
+module "db_sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+
+  name        = "db-sg"
+  description = "Security group for db servers in VPC"
+  vpc_id      = module.vpc.vpc_id
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "mysql-tcp"
+      source_security_group_id = module.app_server_sg.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+}  
